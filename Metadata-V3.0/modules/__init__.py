73211b2b099fbd8fb4ac8807d848a94eb63700fe
fairsailau <fairsailau@gmail.com>
2025-04-18 12:10:29 +1000

    Delete Metadata-V3.0 directory

--- Metadata-V3.0/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import os
-
-def create_init_file():
-    """
-    Create an __init__.py file to make the modules directory a proper package
-    """
-    return "# Box AI Metadata Extraction App modules"
