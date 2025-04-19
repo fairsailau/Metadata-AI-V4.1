e7ca50c9f68fc52655bc01522bd7b7e62f0010d1
fairsailau <fairsailau@gmail.com>
2025-04-20 09:55:54 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ from modules.file_browser import file_browser
 from modules.metadata_config import metadata_config
 from modules.processing import process_files
 from modules.results_viewer import view_results
-from modules.direct_metadata_application_enhanced import apply_metadata_direct as apply_metadata
+from modules.direct_metadata_application_enhanced_fixed import apply_metadata_direct as apply_metadata
 from modules.document_categorization import document_categorization
 from modules.metadata_template_retrieval import get_metadata_templates, initialize_template_state
 
